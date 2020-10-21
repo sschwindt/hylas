7b537ef5b62eca2e385b1bd74b887e68cf114112
sschwindt <sebastian.schwindt@alumni.epfl.ch>
2020-10-21 14:21:02 +0200

    adapted readthedocs config

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
     'sphinxcontrib.httpdomain',
     'sphinx_rtd_theme',
 ]
